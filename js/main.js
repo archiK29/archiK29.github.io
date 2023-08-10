5eba7b5291d11f40fdfc33398b78005d64049660
archiK29 <136885217+archiK29@users.noreply.github.com>
2023-08-10 14:16:27 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,4 @@
+    $('.header-top__burger').on('click', function(){
+        $('.header-top__burger').toggleClass('header-top__burger--active');
+        $('.header-top__list').toggleClass('header-top__list--active');
+    });
